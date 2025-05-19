f4adaf48ca323852807affd951d120a66a995d9d
Deshawn10k <deshawnkawesa@gmail.com>
2025-05-19 12:09:25 +0200

    wijziging: wachtwoord controle toegevoegd, zodra het ww goed is, dan doorsturen naar dashboard.

--- php/login.php
+++ php/login.php
@@ -2,26 +2,33 @@
 session_start();
 require 'db.php';
 
-$username = $_POST['username'];
-$password = $_POST['password'];
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-$stmt = $conn->prepare("SELECT id, password FROM users WHERE username = ?");
-$stmt->bind_param("s", $username);
-$stmt->execute();
-$stmt->store_result();
+    
+    $sql = "SELECT id, password FROM users WHERE username = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-if ($stmt->num_rows === 1) {
-    $stmt->bind_result($id, $hashed_pw);
-    $stmt->fetch();
-    if (password_verify($password, $hashed_pw)) {
-        $_SESSION['user_id'] = $id;
-        $_SESSION['username'] = $username;
-        header("Location: ../dashboard.php");
-        exit;
+    if ($result->num_rows === 1) {
+        $user = $result->fetch_assoc();
+
+        if (password_verify($password, $user['password'])) {
+       
+            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['username'] = $username;
+
+         
+            header('Location: dashboard.php');
+            exit();
+        } else {
+            echo "Wachtwoord incorrect";
+        }
     } else {
-        echo "Verkeerde inloggegevens.";
+        echo "Gebruiker niet gevonden";
     }
-} else {
-    echo "Gebruiker niet gevonden.";
 }
 ?>
